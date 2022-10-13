1f20da2e02189a68eac40b9db3a42254f33c60f3
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-10-13 16:56:37 -0300

    hotfix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './infra/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on port ${process.env.PORT || 3000}`);
   });
