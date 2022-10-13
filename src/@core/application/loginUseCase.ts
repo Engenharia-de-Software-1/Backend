30b3246ac3d2bf8fa5e8f5018a6ac99dc1f4d597
Nuisigor <dexchanel@gmail.com>
2022-10-13 15:21:24 -0300

    feat: created admin login
    Co-authored-by: getuliobr <getuliobr@users.noreply.github.com>

--- src/@core/application/loginUseCase.ts
+++ src/@core/application/loginUseCase.ts
@@ -2,6 +2,7 @@ import { ILoginInput, ILoginOutput } from './../domain/dtos/LoginDTO';
 import { IHashRepository } from './../domain/repositories/IHashRepository';
 import { IUserRepository } from './../domain/repositories/IUserRepository';
 import { IJwtRepository } from '../domain/repositories/Auth/IJwtRepository';
+import { IAdminRepository } from '../domain/repositories/IAdminRepository';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: '.env.local' });
@@ -11,25 +12,29 @@ export class LoginUseCase {
     private jwtTokenService: IJwtRepository,
     private userRepository: IUserRepository,
     private hashRepository: IHashRepository,
+    private adminRepository: IAdminRepository
   ) {}
 
   async execute(input: ILoginInput): Promise<ILoginOutput> {
     const { email, password } = input;
 
-    const user = await this.userRepository.findByEmail(email);
-    if (!user) {
+    const user = await this.userRepository.findByEmail(email, true);
+    const admin = await this.adminRepository.findByEmail(email, true);
+    if (!user && !admin) {
       throw new Error('Cant find user or password is wrong');
     }
 
+    const userFinded = user ? user : admin;
+
     const match = await this.hashRepository.compareHash(
       password,
-      user.password,
+      userFinded.password,
     );
     if (!match) {
       throw new Error('Cant find user or password is wrong');
     }
 
-    const payload = { userId: user.id };
+    const payload = { userId: userFinded.id };
     const secret = process.env.JWT_SECRET;
     const expiresIn = process.env.JWT_EXPIRARTION_TIME + 's';
     const token = this.jwtTokenService.createToken(payload, secret, expiresIn);
