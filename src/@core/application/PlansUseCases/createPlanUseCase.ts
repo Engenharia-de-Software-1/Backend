d2279b9f39e11576f9040fc5cc5c138707f59271
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-11-19 19:22:23 -0300

    fix: return null when not finding a plan

--- src/@core/application/PlansUseCases/createPlanUseCase.ts
+++ src/@core/application/PlansUseCases/createPlanUseCase.ts
@@ -1,30 +1,33 @@
 import { UpdatePlanUseCase } from './updatePlanUseCase';
 import { Plans } from './../../domain/entities/plans.entity';
-import { IPlansInput, IPlansOutput, IPlansUpdate } from "src/@core/domain/dtos/PlansDTO";
-import { IPlansRepository } from "src/@core/domain/repositories/IPlansRepository";
+import {
+  IPlansInput,
+  IPlansOutput,
+  IPlansUpdate,
+} from 'src/@core/domain/dtos/PlansDTO';
+import { IPlansRepository } from 'src/@core/domain/repositories/IPlansRepository';
 
+export class CreatePlanUseCase {
+  constructor(
+    private plansRepository: IPlansRepository,
+    private updatePlanUseCase: UpdatePlanUseCase,
+  ) {}
 
-export class CreatePlanUseCase{
-    constructor(
-        private plansRepository: IPlansRepository,
-        private updatePlanUseCase: UpdatePlanUseCase
-    ){}
-
-    async execute(data: IPlansInput): Promise<IPlansOutput>{
-        if(this.plansRepository.findByUserId(data.userId)){
-            const plan = await this.plansRepository.findByUserId(data.userId);
-            const today = new Date();
-            const expirationDate = new Date(today.setDate(today.getDate() + 30));
-            const planData : IPlansUpdate = {
-                plan: data.plan,
-                expirationDate: expirationDate
-            }
-            return this.updatePlanUseCase.execute(plan.userId, plan.id, planData);
-        }
-
-        const plan = Plans.create(data);
-        await this.plansRepository.insert(plan);
-        
-        return plan;
+  async execute(data: IPlansInput): Promise<IPlansOutput> {
+    let plan = await this.plansRepository.findByUserId(data.userId, true);
+    if (plan) {
+      const today = new Date();
+      const expirationDate = new Date(today.setDate(today.getDate() + 30));
+      const planData: IPlansUpdate = {
+        plan: data.plan,
+        expirationDate: expirationDate,
+      };
+      return this.updatePlanUseCase.execute(plan.userId, plan.id, planData);
     }
+
+    plan = Plans.create(data);
+    await this.plansRepository.insert(plan);
+
+    return plan;
+  }
 }
